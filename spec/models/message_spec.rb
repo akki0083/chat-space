ec393966816ed3f00ae89df7eb434af9593920e7
akki0083 <fighters.nkt@gmail.com>
2019-11-29 17:35:02 +0900

    保存できた場合と保存できなかった時を場合分け

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,29 +1,38 @@
+    context 'can save' do
+
       # テキストと画像があれば保存できる
       it "is valid with a content, image" do
         expect(build(:message)).to be_valid
       end
+
       # テキストがあれば保存できる
       it "is valid with a content" do
         expect(build(:message, content: nil)).to be_valid
       end
+
       # 画像があれば保存できる
       it "is valid with a image" do
         expect(build(:message, image: nil)).to be_valid
       end
     end
+
+    context 'can not save' do
       # テキストと画像がないと保存できない
       it "is invalid without a content and image" do
         message = build(:message, content: nil, image: nil)
         message.valid?
         expect(message.errors[:content]).to include("を入力してください")
       end
+
       it 'is invalid without group_id' do
         message = build(:message, group_id: nil)
         message.valid?
         expect(message.errors[:group]).to include('を入力してください')
       end
+
       it 'is invalid without user_id' do
         message = build(:message, user_id: nil)
         message.valid?
         expect(message.errors[:user]).to include('を入力してください')
       end
+    end
