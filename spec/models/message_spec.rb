2643757c1eb669bd51870dbf2e50855db35be9c7
akki0083 <fighters.nkt@gmail.com>
2019-11-29 17:32:24 +0900

    テキストがあれば保存できる

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,3 +2,7 @@
       it "is valid with a content, image" do
         expect(build(:message)).to be_valid
       end
+      # テキストがあれば保存できる
+      it "is valid with a content" do
+        expect(build(:message, content: nil)).to be_valid
+      end
