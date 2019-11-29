5b0038eba8a5b4ff904234b764a86a4e25ae2f27
akki0083 <fighters.nkt@gmail.com>
2019-11-29 17:39:04 +0900

    rspecでdeviseを使う準備

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,1 +1,4 @@
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
   config.include FactoryBot::Syntax::Methods
