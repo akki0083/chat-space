669c758f4b33f4430f659413da60aae884caebc0
akki0083 <fighters.nkt@gmail.com>
2019-12-03 19:48:36 +0900

    不要になった記述を削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,32 +1,9 @@
 $(function() {
   function buildHTML(message) {
-    if (message.image.url) {
-      var html = `<div class="contents__log">
-                    <div class="contents__log__name">
-                      ${message.name}
-                    </div>
-                    <div class="contents__log__time">
-                      ${message.time}
-                    </div>
-                  </div>
-                    <p class="contents__message">
-                      ${message.content}
-                    </p>
-                  <img class="contents__image" src=${message.image.url}>`
-    } else {
-      var html = `<div class="contents__log">
-                   <div class="contents__log__name">
     var img = message.image ? `<img class="contents__image" src=${message.image.url}>` : "";
                     ${message.name}
-                   </div>
-                   <div class="contents__log__time">
-                   ${message.time}
                   </div>
                   </div>
-                  <p class="contents__message">
-                    ${message.content}
-                  </p>`
-    };
                  ${img}`
     return html;
   };
