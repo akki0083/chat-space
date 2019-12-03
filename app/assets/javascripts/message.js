af825bdf330db43489f4ad4bad709532406de144
akki0083 <fighters.nkt@gmail.com>
2019-12-03 19:50:16 +0900

    インデントを調整

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,17 @@
 $(function() {
   function buildHTML(message) {
     var img = message.image ? `<img class="contents__image" src=${message.image.url}>` : "";
+    var html  = `<div class="contents__log">
+                  <div class="contents__log__name">
                     ${message.name}
                   </div>
+                  <div class="contents__log__time">
+                    ${message.time}
                   </div>
+                 </div>
+                 <p class="contents__message">
+                  ${message.content}
+                 </p>
                  ${img}`
     return html;
   };
