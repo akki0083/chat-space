ef95b03ad32400ce11a5c5cc1ef3ac93724d226f
akki0083 <fighters.nkt@gmail.com>
2019-12-04 14:03:07 +0900

    削除ボタンが押された時のHTML

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -19,6 +19,15 @@ $(function(){
     $("#user-search-result").append(html);
   }
 
+  function appendDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-append-user").append(html);
+  }
+
   $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();
 
