6d98f17d0c2afdca79f31070a7200a43addd2446
akki0083 <fighters.nkt@gmail.com>
2019-12-05 16:28:42 +0900

    新しく作った親要素のクラス名を子要素に反映

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,19 +1,22 @@
 $(function() {
   function buildHTML(message) {
-    var img = message.image.url ? `<img class="contents__image" src=${message.image.url}>` : "";
-    var html  = `<div class="contents__log">
-                  <div class="contents__log__name">
-                    ${message.name}
+    var img = message.image.url ? `<img class="contents__content__image" src=${message.image.url}>` : "";
+    var html  = `
                 <div class="contents__content" data-message-id="${message.id}">
+                  <div class="contents__content__log">
+                    <div class="contents__content__log__name">
+                      ${message.name}
+                    </div>
+                    <div class="contents__content__log__time">
+                      ${message.time}
+                    </div>
                   </div>
-                  <div class="contents__log__time">
-                    ${message.time}
-                  </div>
-                 </div>
-                 <p class="contents__message">
-                  ${message.content}
-                 </p>
-                 ${img}`
+                  <p class="contents__content__message">
+                    ${message.content}
+                  </p>
+                  ${img}
+                </div>
+                `
     return html;
   };
 
