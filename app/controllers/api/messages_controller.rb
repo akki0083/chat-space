cf227e41feff177356846f79a4944e7481081af1
akki0083 <fighters.nkt@gmail.com>
2019-12-05 16:10:08 +0900

    最後のメッセージのidを取得

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,5 +2,7 @@ class Api::MessagesController < ApplicationController
   def index
     # ルーティングでの設定によりparamsの中にgroup_idというキーでグループのidが入るので、これを元にDBからグループを取得する
     group = Group.find(params[:group_id])
+    # ajaxで送られてくる最後のメッセージのid番号を変数に代入
+    last_message_id = params[:id].to_i
   end
 end
